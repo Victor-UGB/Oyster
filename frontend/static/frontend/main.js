e1683e54af97570170adfe45f966d8a9e45df732
Victor-UGB <76266429+Victor-UGB@users.noreply.github.com>
2023-05-12 01:34:46 +0100

    new commit

---
+++ frontend/static/frontend/main.js
@@ -0,0 +1,2 @@
+/*! For license information please see main.js.LICENSE.txt */
+(()=>{var __webpack_modules__={"./src/index.js":()=>{eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'C:\\\\Users\\\\hp\\\\Desktop\\\\pearlsonline\\\\frontend\\\\src\\\\index.js'\");\n\n//# sourceURL=webpack://frontend/./src/index.js?")}},__webpack_exports__={};__webpack_modules__["./src/index.js"]()})();
